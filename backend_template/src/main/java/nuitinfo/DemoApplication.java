95b419d40cb205fdd3b2f301a3655430dee77b34
Walid LARABI <walid.larabi@etu.unice.fr>
2019-12-05 20:42:00 +0100

    changes

---
+++ backend_template/src/main/java/nuitinfo/DemoApplication.java
@@ -0,0 +1,16 @@
+package nuitinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(DemoApplication.class, args);
+		new Sender().send("Hello Spring Kafka!");
+
+	}
+
+}
