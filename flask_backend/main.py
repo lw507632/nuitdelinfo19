8fd2cc9f80fe2d804dc8bffd3bc627948a50f7a3
root <root@vps612309.ovh.net>
2019-12-06 02:31:21 +0100

    correcting

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -20,22 +20,21 @@ def oui_non():
     resp = Response(js, status=200, mimetype='application/json')
     return resp
 
-@app.route("/aides", methods=["Post"])
+@app.route("/aides", methods=["POST"])
 def aides():
+    
+
     if request.method == "POST":
         req_data = request.get_json()
-        res = []
+        tmp = set()
+        res = {'aides':[]}
         for k in aides_mock.keys():
             for key, item in aides_mock[k].items():
                 for key2, item2 in req_data.items():
-                    if item==item: res.append(key)
-
+                    if item==item2:
+                        tmp.add(k)
+        res['aides'].extend(list(tmp))
         return res
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run()
